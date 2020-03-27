c6c0cb0fb7329ed1df09aaea62b133de4edac00a
lin <yuyang@udel.edu>
2020-03-26 22:45:19 -0400

    delete cmake-build-debug

--- rotors_description/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lin/Data/project/catkin_ws_rotors/src/rotors_simulator/rotors_description
-# Build directory: /home/lin/Data/project/catkin_ws_rotors/src/rotors_simulator/rotors_description/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
