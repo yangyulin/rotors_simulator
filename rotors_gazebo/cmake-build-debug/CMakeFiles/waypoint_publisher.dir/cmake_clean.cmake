08ec20337b812ffb3d09bc25e6512ba95470ea6a
lin <yuyang@udel.edu>
2020-03-26 22:43:25 -0400

    modify the files

---
+++ rotors_gazebo/cmake-build-debug/CMakeFiles/waypoint_publisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/waypoint_publisher.dir/src/waypoint_publisher.cpp.o"
+  "devel/lib/rotors_gazebo/waypoint_publisher"
+  "devel/lib/rotors_gazebo/waypoint_publisher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/waypoint_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
